4a46d937b1e211e5d3b161996ff2d58e229b2d7c
SammyGbabs <s.babalola@alustudent.com>
2024-11-29 11:03:07 +0200

    fix: binding port

--- API/main.py
+++ API/main.py
@@ -17,4 +17,5 @@ def read_root():
     return {"message": "Welcome to the Maize Leaf Disease Detection API"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Default to port 8000 if not set
+    uvicorn.run(app, host="0.0.0.0", port=port)
